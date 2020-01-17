081ee6a80f050084a719e7c3b264ac36cd3e4a32
Romain Tello <romain.tello@gmail.com>
2020-01-17 15:48:19 +0100

    Add first Axios request

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import Axios from 'axios';
+
+const fetchFromAPI = () => {
+  Axios.get('https://swapi.co/api/films/1/')
+  .then(response => console.log(response))
+  ;
+}
 
 const App = () =>
   <div>
+    {fetchFromAPI()}
     Bonjour Grenoble!
   </div>
 ;
